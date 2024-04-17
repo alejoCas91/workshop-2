a13c14a0daefd93a7a3977f9efd1db66f2ac496e
alejoCas91 <alejocastrillon103@gmail.com>
2024-04-16 20:34:30 -0500

    first challenge

---
+++ Section 1/reto 1/app.js
@@ -0,0 +1,17 @@
+const destination = prompt("Where you go?: ");
+const dailyBudget = prompt("How much will you spend per day (Approximately)?: ");
+let Instance, initialBudget, travelBudget;
+
+Instance = prompt("how many days will you stay?: ");
+console.log(`You stay for:`, Instance);
+
+initialBudget = prompt("Your budget: ");
+console.log( `Tu presupuesto es de: `, initialBudget);
+
+travelBudget = initialBudget / Instance;
+
+if (travelBudget >= dailyBudget) {
+  alert("Julian can travel");
+} else {
+  alert("insufficient funds ●︿● save more money");
+}
