223147acd68dd438d97bc9feebccbce97a3e3c00
alejoCas91 <161416106+alejoCas91@users.noreply.github.com>
2024-04-19 14:57:08 -0500

    Add files via upload
    Third challenge section 2

--- Section 2/reto 3/app.js
+++ Section 2/reto 3/app.js
@@ -1,3 +1,32 @@
-const weather = prompt("Cómo ves el clima de hoy? A) Soleado B) Nublado C) Lluvioso")
-const Space = 0
-const Weight = 10
+const clima = prompt("Ingresa el pronóstico del clima (e.j. soleado, lluvioso, etc.)");
+const espacioDisponible = parseInt(prompt("Ingresa el espacio disponible en tu maleta en kg"));
+const pesoTotalPermitido = parseInt(prompt("Ingresa el límite de peso de tu equipaje en kg"));
+
+const artículos = [
+    {nombre: "paraguas", peso: 1, clima: ["lluvioso"]},
+    {nombre: "cámara", peso: 0.5, clima: ["soleado"]},
+    {nombre: "libro", peso: 0.3, clima: ["todo"]},
+    {nombre: "tableta", peso: 0.7, clima: ["todo"]},
+    {nombre: "auriculares", peso: 0.1, clima: ["todo"]}
+];
+
+function decidirArticulos(clima, espacioDisponible, pesoTotalPermitido, artículos) {
+    let articulosAgregados = [];
+
+    for (let i = 0; i < artículos.length; i++) {
+        if (artículos[i].clima.includes(clima) && espacioDisponible >= artículos[i].peso && pesoTotalPermitido >= artículos[i].peso) {
+            articulosAgregados.push(artículos[i].nombre);
+            espacioDisponible -= artículos[i].peso;
+            pesoTotalPermitido -= artículos[i].peso;
+        }
+    }
+
+    return articulosAgregados;
+}
+let artículosAgregados = decidirArticulos(clima, espacioDisponible, pesoTotalPermitido, artículos);
+
+if (artículosAgregados.length > 0) {
+    alert("Es aconsejable llevar los siguientes artículos: " + artículosAgregados.join(", "));
+} else {
+    alert("No se recomienda llevar artículos adicionales.");
+}
